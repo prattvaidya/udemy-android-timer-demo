15e5321a16a4b732a03cf54087cacd093cc9444b
Pratt Vaidya <pratt.vaidya@ttt.studio>
2021-10-14 17:51:34 -0700

    Timer with a Handler and a Runnable

--- app/src/main/java/ttt/prattvaidya/timerdemo/MainActivity.java
+++ app/src/main/java/ttt/prattvaidya/timerdemo/MainActivity.java
@@ -3,6 +3,8 @@ package ttt.prattvaidya.timerdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                Log.i("Hey c'est us", "A second passed by");
+
+                handler.postDelayed(this, 1000);
+            }
+        };
+        handler.post(run);
     }
 }
